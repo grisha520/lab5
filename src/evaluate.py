bf21f70b94a4eb9ef149fa09af1d4a630d2443a7
Alegri <79797405+grisha520@users.noreply.github.com>
2025-05-06 15:26:41 +0500

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,4 +1,3 @@
-# src/evaluate.py
 import pandas as pd
 import yaml
 import json
@@ -8,22 +7,17 @@ from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 with open("params.yaml", "r") as f:
     params = yaml.safe_load(f)["evaluate"]
 
-# Загрузка модели и данных
 model = joblib.load(params["input_path"])
 df = pd.read_csv(params["test_data"])
 
-# Целевая переменная - Survived (0 = не выжил, 1 = выжил)
 y_true = df["Survived"]
 
-# Признаки - все числовые столбцы, кроме Survived
 numeric_cols = df.select_dtypes(include=['number']).columns.drop("Survived")
 X = df[numeric_cols]
 
-# Предсказания
 y_pred = model.predict(X)
-y_proba = model.predict_proba(X)[:, 1]  # Вероятность класса 1 (выжил)
+y_proba = model.predict_proba(X)[:, 1]  
 
-# Вычисление метрик
 metrics = {}
 if "accuracy" in params["params"]["metrics"]:
     metrics["accuracy"] = accuracy_score(y_true, y_pred)
@@ -32,6 +26,5 @@ if "f1" in params["params"]["metrics"]:
 if "roc_auc" in params["params"]["metrics"]:
     metrics["roc_auc"] = roc_auc_score(y_true, y_proba)
 
-# Сохранение метрик
 with open(params["output_path"], "w") as f:
     json.dump(metrics, f, indent=4)
