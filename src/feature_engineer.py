1e5e7d2bd8b94b10c2718b90620a6f66e52ec29b
Alegri <79797405+grisha520@users.noreply.github.com>
2025-05-06 15:26:56 +0500

    Update feature_engineer.py

--- src/feature_engineer.py
+++ src/feature_engineer.py
@@ -1,4 +1,3 @@
-# src/feature_engineer.py
 import pandas as pd
 import yaml
 
@@ -7,14 +6,10 @@ with open("params.yaml", "r") as f:
 
 df = pd.read_csv(params["input_path"])
 
-# Генерация новых признаков
 if "ratio_feature" in params["params"]["new_features"]:
-    # Отношение возраста к цене билета (защита от деления на 0)
     df["age_to_fare_ratio"] = df["Age"] / (df["Fare"] + 1e-6)
 
 if "interaction_feature" in params["params"]["new_features"]:
-    # Взаимодействие класса и количества родственников на борту
     df["class_family_interaction"] = df["Pclass"] * (df["Siblings/Spouses Aboard"] + df["Parents/Children Aboard"])
 
-# Сохранение
 df.to_csv(params["output_path"], index=False)
