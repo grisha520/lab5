286abd5b219c8fe7314bf742d2fee130b3458135
Alegri <79797405+grisha520@users.noreply.github.com>
2025-05-06 15:26:11 +0500

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,4 +1,3 @@
-# src/train_model.py
 import pandas as pd
 import yaml
 import joblib
@@ -7,22 +6,17 @@ from sklearn.ensemble import RandomForestClassifier
 with open("params.yaml", "r") as f:
     params = yaml.safe_load(f)["train"]
 
-# Загрузка данных
 df = pd.read_csv(params["input_path"])
 
-# Целевая переменная - Survived (0 = не выжил, 1 = выжил)
 y = df["Survived"]
 
-# Признаки - все числовые столбцы, кроме Survived
 numeric_cols = df.select_dtypes(include=['number']).columns.drop("Survived")
 X = df[numeric_cols]
 
-# Обучение модели
 model = RandomForestClassifier(
     n_estimators=params["params"]["n_estimators"],
     random_state=params["params"]["random_state"]
 )
 model.fit(X, y)
 
-# Сохранение модели
 joblib.dump(model, params["output_path"])
